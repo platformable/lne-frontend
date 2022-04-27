cc0b4e5215190cf10fde9e7de2c44e7e393393d0
Alexei Garban <garban.valdeon@gmail.com>
2022-04-27 15:32:19 +0200

    fix profile problem

--- pages/clients/[clientid]/profile/index.js
+++ pages/clients/[clientid]/profile/index.js
@@ -6,7 +6,11 @@ export default function ClientProfilePage({data}) {
     console.log("data",data)
   return (
      <Layout>
-    <div>Client Profile Page</div>
+         <div className="container mx-auto my-5">
+    <h3 className="font-black">Client Profile Page</h3>
+    <h3>Client Name: {data[0].clientfirstname}</h3>
+    <h3>Client Lastname: {data[0].clientlastname}</h3>
+    </div>
     </Layout>
   )
 }
@@ -16,7 +20,7 @@ export const getServerSideProps = withPageAuthRequired({
     async getServerSideProps(ctx) {
     
      let {clientid} = ctx.params
-      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/${clientid}`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/clients/${clientid}`);
 
       const data = await res.json();
       return { props: { data } };
