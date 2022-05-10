321838b6e531687d5b1c632719583a4d8a2c68cd
leoncan122 <leon.can122@gmail.com>
2022-05-10 22:32:59 +0200

    modify client/profile style, add ImpactBaselineModal

--- pages/clients/[clientid]/profile/index.js
+++ pages/clients/[clientid]/profile/index.js
@@ -2,76 +2,154 @@ import React from "react";
 import Layout from "../../../../components/Layout";
 import { useUser, getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Link from "next/link";
+import Image from "next/image";
+
+import infoIcon from "../../../../public/client/info-icon.svg"
+import userIcon from "../../../../public/client/user-icon.svg"
+import itemMenuActionsImg from "../../../../public/client/item-menu-actions.svg"
+
+
 
 export default function ClientProfilePage({ data }) {
   console.log("data", data);
   return (
     <Layout>
-      <section id="client-profile-page-client-information" className="my-5">
-      <h3 className="font-black text-center">Client Profile</h3>
-        <div className="container mx-auto my-5 grid grid-cols-2 gap-5">
-
-    <div className="text-center"> 
-          <h3>Client: {data[0].clientfirstname}{' '}{data[0].clientlastname.charAt(0)}</h3>
-          
-          </div>
-          <div className="text-center">
-          <h3>HCW:{data[0].clienthcwname}{' '} {data[0].clienthcwlastname}</h3>
-
-          </div>
+      <section id="client-profile-page-client-information" className="my-10 container mx-auto px-8 md:px-12 lx:px-4 ">
+        <div className='user-box-container p-3 flex justify-around items-center  font-bold'>
+              <Image src={userIcon} className="mr-4"></Image>
+              <p className='text-blue-500'>{data[0].clientid}</p>
         </div>
+        <article className='container mx-auto border-2 border-blue-400 rounded-xl rounded-tl-none text-black md:grid md:grid-cols-2 font-bold shadow-xl'>
+            
+            <div className='grid  gap-2 font-semibold border-b border-b-blue-500 py-11 px-10 
+             md:pl-7  md:border-b-0 md:border-r  md:border-r-blue-500 lg:pr-16'>
+                    
+                <div className='grid mt-4 grid-rows-2 md:flex md:items-center md:justify-between'>
+                <p className="">Date Client Joined LNE</p>
+                <p className='justify-self-end'>MM/DD/YY</p>
+                </div>
+                <hr className='border-blue-400'></hr>
+                
+                <div className='grid grid-rows-2 md:flex md:items-center md:justify-between'>
+                <p  className="">Date Of Last Action</p>
+                <p className='justify-self-end'>MM/DD/YY</p>
+                </div>
+                <hr className='border-blue-400 hidden md:block'></hr>
+
+            </div>
+            
+            <div className='py-10 pl-4 md:pl-10  grid grid-rows-3 gap-5 items-center justify-items-start'>
+                <div className='flex'>
+                    <Image src={infoIcon} ></Image>
+                    <p className='px-4 '>You need to complete x form</p>
+                </div>
+                <div className='flex'>
+                    <Image src={infoIcon} ></Image>
+                    <p className='px-4'>Service action plan (reminders)</p>
+                </div>
+                <div className='flex'>
+                    <Image src={infoIcon} ></Image>
+                    <p className='px-4'>Tickler style updates</p>
+                </div>
+            </div>   
+        </article>
+                {/* <div className="container mx-auto my-5 grid grid-cols-2 gap-5">
+
+            <div className="text-center"> 
+                  <h3>Client: {data[0].clientfirstname}{' '}{data[0].clientlastname.charAt(0)}</h3>
+                  
+                  </div>
+                  <div className="text-center">
+                  <h3>HCW:{data[0].clienthcwname}{' '} {data[0].clienthcwlastname}</h3>
+
+                  </div>
+                </div> */}
       </section>
-      <section id="client-profile-page-navigation" className="my-5">
-        <div className="container mx-auto bg-light-blue rounded-xl p-5">
-          <div className="client-profile-page-navigation-container">
+      <section id="client-profile-page-navigation" className="mt-5 font-bold w-screen">
+          
+        <div className='text-black bg-light-blue py-12 px-10 md:px-0 lg:px-9 xl:px-20 w-full'>
+            <h1 className='p-3  text-center md:text-left md:pl-12 lg:pl-4 '>What do you want to do today?</h1>
+            <ul className='grid justify-center  mt-4
+             text-center gap-8 md:grid-cols-3 md:border-0 lg:grid-cols-3 xl:grid-cols-6'>
+
+                <li className=''>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl p-2 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          MSA <br /> DOCUMENTATION
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+
+                <li className=''>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          IMPACT <br /> TRACKING
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+
+                <li className=' '>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          ADD A <br /> PROGRESS NOTE
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+
+                <li className=''>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          SERVICE <br /> ACTION PLAN
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+
+                <li className=''>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          AIRS <br /> FORMS
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+                
+                <li className=''>
+                    <Link href={`/clients/${data[0].clientid}/service-action-plan`}>
+                      <div className="w-36 shadow-2xl client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+                        <Image src={itemMenuActionsImg}></Image>
+                        <h6 className="text-center">
+                          LNE <br /> FORMS
+                        </h6>
+                      </div>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+          
+          
+          {/* <div className="client-profile-page-navigation-container">
             <Link href={`/clients/${data[0].clientid}/service-action-plan`}><div className="client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
               <h4 className="text-center">
                 Service <br /> Action Plan
               </h4>
-              <svg
-                width="64"
-                height="84"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4 21.4V2.6C4 2.26863 4.26863 2 4.6 2H16.2515C16.4106 2 16.5632 2.06321 16.6757 2.17574L19.8243 5.32426C19.9368 5.43679 20 5.5894 20 5.74853V21.4C20 21.7314 19.7314 22 19.4 22H4.6C4.26863 22 4 21.7314 4 21.4Z"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M16 5.4V2.35355C16 2.15829 16.1583 2 16.3536 2C16.4473 2 16.5372 2.03725 16.6036 2.10355L19.8964 5.39645C19.9628 5.46275 20 5.55268 20 5.64645C20 5.84171 19.8417 6 19.6464 6H16.6C16.2686 6 16 5.73137 16 5.4Z"
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M8 10L16 10"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M8 18L16 18"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M8 14L12 14"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+              
             </div>
             </Link>
-          </div>
-        </div>
+          </div> */}
       </section>
     </Layout>
   );
