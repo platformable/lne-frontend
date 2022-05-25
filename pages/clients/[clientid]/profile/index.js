f2097f05d4ec1d51f4980c360a6ca76c90ecbd77
Alexei Garban <garban.valdeon@gmail.com>
2022-05-25 11:39:06 +0200

    new page added

--- pages/clients/[clientid]/profile/index.js
+++ pages/clients/[clientid]/profile/index.js
@@ -6,6 +6,9 @@ import { useRouter } from "next/router";
 
 export default function ClientProfilePage({ data }) {
 
+  console.log("profile page data:",data)
+  
+
   const { user, error, isLoading } = useUser();
   const loggedUserRole = user && user["https://lanuevatest.herokuapp.com/roles"];
 
@@ -43,7 +46,7 @@ export default function ClientProfilePage({ data }) {
         <div className="container mx-auto bg-light-blue rounded-xl p-5">
           <div className="client-profile-page-navigation-container grid grid-cols-6 gap-5">
             {loggedUserRole==="DES" ? (
-              <Link href={data[0]?.msa_form_id ?`/clients/${data[0]?.clientid}/msa_form/des_msa_form_edit`:`/clients/${data[0]?.clientid}/msa_form`}>
+              <Link href={data[0]?.msaformid ?`/clients/${data[0]?.clientid}/msa_form/des_msa_form_edit`:`/clients/${data[0]?.clientid}/msa_form`}>
               <div className="client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
               <div className="flex justify-center">
               <svg width="64" height="84" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -55,13 +58,13 @@ export default function ClientProfilePage({ data }) {
               </svg>
               </div>
               <h4 className="text-center">
-                {data[0]?.msa_form_id ===undefined || data[0]?.msa_form_id==="" || data[0]?.msa_form_id===null ? (`Create MSA Form`) : 'Edit MSA Form' }
+                {data[0]?.msaformid ===undefined || data[0]?.msaformid==="" || data[0]?.msaformid===null ? (`Create MSA Form`) : 'Edit MSA Form' }
               </h4>
               </div>
               </Link>
             ):
             (
-              <Link href={data[0]?.msa_form_id ?`/clients/${data[0]?.clientid}/msa_form/edit`:`/clients/${data[0]?.clientid}/msa_form`}>
+              <Link href={data[0]?.msaformid ?`/clients/${data[0]?.clientid}/msa_form/edit`:`/clients/${data[0]?.clientid}/msa_form`}>
               <div className="client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
               <div className="flex justify-center">
               <svg width="64" height="84" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -73,7 +76,7 @@ export default function ClientProfilePage({ data }) {
               </svg>
               </div>
               <h4 className="text-center">
-                {data[0]?.msa_form_id ===undefined || data[0]?.msa_form_id==="" || data[0]?.msa_form_id===null ? (`Create MSA Form`) : 'Edit MSA Form' }
+                {data[0]?.msaformid ===undefined || data[0]?.msaformid==="" || data[0]?.msaformid===null ? (`Create MSA Form`) : 'Edit MSA Form' }
               </h4>
               </div>
               </Link>
@@ -84,8 +87,8 @@ export default function ClientProfilePage({ data }) {
             
          
 
-            {data[0]?.airsintakeform==="1" && data[0]?.comprehensiveriskbehaviorassessment==="1" ? 
-           <Link href={data[0].serviceactionplan==="0" ?`/clients/${data[0]?.clientid}/service-action-plan` : `/clients/${data[0]?.clientid}/service-action-plan/edit`}>
+            {data[0]?.msaformairsintakeform==="1" && data[0]?.msaformcomprehensiveriskbehavrioassesment==="1" ? 
+           <Link href={data[0]?.servicesactionplanid ?`/clients/${data[0]?.clientid}/service-action-plan/edit` : `/clients/${data[0]?.clientid}/service-action-plan`}>
            <div className="client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
            
            <div className="flex justify-center">
@@ -131,11 +134,67 @@ export default function ClientProfilePage({ data }) {
            </svg>
            </div>
            <h4 className="text-center">
-           {data[0]?.serviceactionplan ==="0" ? "Create Service Action Plan" : `View Service Action Plan`}
+           {data[0]?.servicesactionplanid  ? "View Service Action Plan" : `Create Service Action Plan`}
            </h4>
          </div>
          </Link>  
           :""}
+
+
+{data[0]?.servicesactionplanid ? (
+  <Link href={`/clients/${data[0]?.clientid}/progress_note`}>
+  <div className="client-profile-page-navigation-icon-container boder-dark-blue bg-white cursor-pointer rounded-xl py-2 px-5 inline-block">
+  
+  <div className="flex justify-center">
+  <svg
+    width="64"
+    height="84"
+    strokeWidth="1.5"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4 21.4V2.6C4 2.26863 4.26863 2 4.6 2H16.2515C16.4106 2 16.5632 2.06321 16.6757 2.17574L19.8243 5.32426C19.9368 5.43679 20 5.5894 20 5.74853V21.4C20 21.7314 19.7314 22 19.4 22H4.6C4.26863 22 4 21.7314 4 21.4Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16 5.4V2.35355C16 2.15829 16.1583 2 16.3536 2C16.4473 2 16.5372 2.03725 16.6036 2.10355L19.8964 5.39645C19.9628 5.46275 20 5.55268 20 5.64645C20 5.84171 19.8417 6 19.6464 6H16.6C16.2686 6 16 5.73137 16 5.4Z"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 10L16 10"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 18L16 18"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 14L12 14"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+  </div>
+  <h4 className="text-center">
+  Create Progress Note
+  </h4>
+</div>
+</Link>  
+):""}
+           
+       
            
             
           </div>
