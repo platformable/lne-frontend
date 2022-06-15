527c9c9925a8cfb0e87e50a875600840823e689f
Alexei Garban <garban.valdeon@gmail.com>
2022-06-15 16:49:40 +0200

    update user status

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const { user, error, isLoading } = useUser();
   /*   const {data} = getSession() */
- 
+  const loggedUserStatus = user && user["https://lanuevatest.herokuapp.com/activestatus"];
   const router = useRouter()
   
 useEffect(()=>{
@@ -36,7 +36,7 @@ console.log(user)
         <div className="container mx-auto h-screen grid justify-center content-center">
           <div className="text-center">
         <h1 className="font-black mb-5">Welcome!</h1>
-        <Link href="/dashboard" ><p className={`${styles.btnIndexPage} btn bg-yellow-300 text-black px-5 py-2 rounded-md test cursor-pointer`}>Login / Register</p></Link>
+        <Link href={ `/dashboard`} ><p className={`${styles.btnIndexPage} btn bg-yellow-300 text-black px-5 py-2 rounded-md test cursor-pointer`}>Login / Register</p></Link>
         {user && <h3 className="my-5 font-black">{user.name}</h3>}
         </div>
         </div>
