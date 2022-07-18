201ad19664cb2519b16b03b582347825c134e806
Alexei Garban <garban.valdeon@gmail.com>
2022-07-18 16:02:03 +0200

    new page added

--- pages/supervisorDashboard.js
+++ pages/supervisorDashboard.js
@@ -280,7 +280,7 @@ if(userid!=="All"){
                 )}
 
                 {loggedUserRole === "Supervisor" && (
-                  <Link href="#">
+                  <Link href="/software">
                     <div className="text-center mr-5">
                       <div className="rounded btn-yellow p-5 text-center shadow-xl   mb-2 rounded-xl">
                         <button id="myBtn">
