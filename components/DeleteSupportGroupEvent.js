08874990dcd22f15fe67af63be995e189540b9b5
leoncan122 <leon.can122@gmail.com>
2022-11-08 16:49:48 +0100

    Add delete modal

---
+++ components/DeleteSupportGroupEvent.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios'
+import Image from 'next/image';
+
+import deleteUserIcon from '../public/delete-user-icon.svg'
+
+const DeleteSupportGroupEvent = ({ setShowDeleteEventModal, showDeleteEventModal,selectedEventToDelete, id}) => {
+    console.log("id",selectedEventToDelete)
+    const router = useRouter()
+    const notifyDeleteMessage = () => {
+        toast.success("Client deleted", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      };
+    const handleAuthUserDelete = ({})=>{
+        axios.delete(`http://localhost:4000/support_groups/delete`,{ data: { id } })
+        
+        .then(response=>{
+            console.log(response)
+            if(response.statusText==='OK'){
+                notifyDeleteMessage()
+                setTimeout(()=>{
+                   router.reload()
+                },1000)
+            }
+        })
+        .catch(error=>console.log(error))
+    };
+
+    return (
+        <div className='modal flex items-center '>
+            <div className='relative max-w-md mx-auto bg-yellow-400 py-7 px-10 rounded-md '>
+            <button
+            className="absolute  top-0 right-0"
+            onClick={() => setShowDeleteEventModal((prev) => !prev)}
+            >
+                <img src="/close-window-icon.svg" alt="close-window" title="close-window_" className="rounded-tr"  width="20" /> 
+            </button>
+                <div className='flex flex-col justify-between items-start h-full'>
+                    <div className='flex items-center mb-5'>
+                        {/* <Image src={deleteUserIcon}/> */}
+                        <p className='font-bold text-xl ml-2'>Delete Event</p>
+                    </div>
+                    <label id="name" className="block font-semibold my-5">
+                        Discussion topic
+                        <input readOnly id="name" value={selectedEventToDelete.supportgrouptopic} className="text-lg rounded-lg bg-yellow-100 block mt-2 p-2 px-3" />
+                    </label>
+                     <p className='self-center text-center text-lg font-semibold '>Are you sure you want <br/>to delete this support group event?</p>
+                    <div className='w-full flex justify-between self-center'>
+                        <button className='text-black font-semibold shadow-md bg-[#23D3AA] hover:text-white hover:bg-green-500 cursor-pointer rounded-md p-1 px-10 mx-1 my-5'
+                        onClick={()=> handleAuthUserDelete()} >Yes</button>
+                        <button className='text-white font-semibold shadow-md bg-black hover:text-white hover:bg-blue-900 cursor-pointer rounded-md p-1 px-10 mx-1 my-5' 
+                            onClick={() => setShowDeleteEventModal((prev) => !prev)}>No
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+    );
+};
+
+export default DeleteSupportGroupEvent;
--- pages/supportGroups/pastEvents.js
+++ pages/supportGroups/pastEvents.js
@@ -1,44 +1,38 @@
 import React, { useState, useRef } from "react";
 import Link from "next/link";
-import Layout from "../../../components/Layout";
+import Layout from "../../components/Layout";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
-// import EventsCardItems from "../../components/events/EventsCardItems";
-import Search from "../../../components/SearchEvents";
-// import DeleteEventModal from "../../components/events/DeleteEventModal";
-// import { searchEventByName } from "../../slices/eventsSearchWordSlice";
 
-// import {
-//   updateStartDate,
-//   updateEndDate,
-// } from "../../slices/eventsCalendarDatesSlice";
 import { useEffect } from "react";
+import BackButton from "../../components/BackButton";
+import BackToDashboardButton from "../../components/BackToDashboardButton";
+import DeleteSupportGroupEvent from "../../components/DeleteSupportGroupEvent";
+import { ToastContainer } from "react-toastify";
 
-const SupportGroups = ({ events }) => {
+const PastEvents = ({ events }) => {
   console.log("events", events)
-//   const eventSearchWord = useSelector(
-//     (state) => state.eventsSearchWord.value.word
-//   );
-//   const [searchWord, setSearchWord] = useState(eventSearchWord || "");
-//   const dispatch = useDispatch();
+
   const { user, error, isLoading } = useUser();
   const [selectedEventToDelete, setSelectedEventToDelete] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState(""); 
 
   const [showDeleteEventModal, setShowDeleteEventModal] = useState(false);
-
-  const loggedUserRole =
-    user && user["https://lanuevatest.herokuapp.com/roles"];
-
-  /*   const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState(""); */
-
   const [dateFilter, setDateFilter] = useState({
     startDate: null,
     endDate: null,
   });
+
+  const loggedUserRole =
+    user && user["https://lanuevatest.herokuapp.com/roles"];
+
+ 
+
   
-  const handleDeleteEvent=(id,eventName)=>{
+  
+  const handleDeleteEvent=(id,topic)=>{
     console.log(id)
-    setSelectedEventToDelete({id:id,eventname:eventName})
+    setSelectedEventToDelete({id:id,supportgrouptopic:topic})
     setShowDeleteEventModal(!showDeleteEventModal)
   }
   const searchFunction = (word) => {
@@ -64,20 +58,21 @@ const SupportGroups = ({ events }) => {
 //   console.log("state", state);
 
   const sortedEventsByDate = events.sort(
-    (a, b) => new Date(b.eventdate) - new Date(a.eventdate)
+    (a, b) => new Date(b.supportmeetingdate) - new Date(a.supportmeetingdate)
   );
-
-  console.log(sortedEventsByDate);
+  
   return (
     <Layout showStatusHeader={true}>
-      {/* <PageTopHeading
-        pageTitle={"Manage existing events"}
-        dashboardBtn={true}
-        backBtn={true}
-      /> */}
+      <ToastContainer  autoClose={3000}/>
+      <div className="container mx-auto my-10">
+        <h1 className="font-black mb-5">Review Past group events</h1>
+        <div className="flex items-center gap-5">
+          <BackButton />
+          <BackToDashboardButton />
+        </div>
+      </div>
 
       <div className="container mx-auto grid  items-center grid-cols-1 container mx-auto md:px-0 px-5 md:mb-5 md:gap-5">
-        <Search searchFunction={searchFunction} />
 
         <div className="block md:flex xl:justify-end md:px-0 lg:col-start-4 py-5 md:py-0  mr-0">
           <h3 className="">Filter by date</h3>
@@ -92,12 +87,12 @@ const SupportGroups = ({ events }) => {
               placeholder="start date"
               onChange={(e) => {
                 setDateFilter({ ...dateFilter, startDate: e.target.value });
-                dispatch(
-                  updateStartDate({ ...dateFilter, startDate: e.target.value })
-                );
+                // dispatch(
+                //   updateStartDate({ ...dateFilter, startDate: e.target.value })
+                // );
               }}
-              defaultValue={startDate}
-              className="border-black rounded-md text-sm w-full"
+              // defaultValue={startDate}
+              className="border-black rounded-md py-1 px-2  w-full"
             />
           </label>
           <h3 className="text-left md:text-center md:py-5 md:py-0 py-5">and</h3>
@@ -107,54 +102,41 @@ const SupportGroups = ({ events }) => {
               placeholder="end date"
               onChange={(e) => {
                 setDateFilter({ ...dateFilter, endDate: e.target.value });
-                dispatch(
-                  updateStartDate({ ...dateFilter, endDate: e.target.value })
-                );
+                // dispatch(
+                //   updateStartDate({ ...dateFilter, endDate: e.target.value })
+                // );
               }}
-              defaultValue={endDate}
-              className="border-black rounded-md  text-sm w-full"
+              // defaultValue={endDate}
+              className="border-black rounded-md  py-1 px-2 w-full"
             />
           </label>
         </div>
       </div>
 
-      <div className="events-cards-container grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 container mx-auto md:px-0 px-5 mb-5 gap-5 md:mt-0 mt-5"></div>
-      {/*  HEAD TABLE  */}
-      <div className={`hidden md:grid ${loggedUserRole === "Supervisor" ? "supervisor-existing-events-head-table" : `existing-events-head-table`} container mx-auto  rounded-t-lg py-3 px-7 bg-black text-white`}>
+      <div className="events-cards-container  bg-light-blue py-7">
+      <div className={`hidden md:grid ${loggedUserRole === "Supervisor" ? "supervisor-existing-events-head-table" : `existing-events-head-table`} container mx-auto gap-5 rounded-t-lg py-3 px-7`}>
         {/* <p className="lg:text-xl font-bold flex items-center ">Program</p> */}
-        <p className="lg:text-xl font-bold flex items-center ">Event name</p>
         <p className="lg:text-xl font-bold flex items-center ">Event date</p>
+        <p className="lg:text-xl font-bold flex items-center ">Name of Group</p>
+        <p className="lg:text-xl font-bold flex items-center justify-center">Discussion topic</p>
+        <p className="lg:text-xl font-bold flex items-center justify-center">View/edit event</p>
+        <p className="lg:text-xl font-bold flex items-center justify-center">Delete event</p>
+
+        
       </div>
 
       <div className="container  mx-auto md:px-0 px-7 mb-10 pb-10 rounded-lg ">
         <div className="events-index-btn-container grid grid-cols-1 gap-3 p-0">
           {sortedEventsByDate &&
             sortedEventsByDate
-              ?.filter((event, index) => {
-                if (
-                  searchWord === "" &&
-                  dateFilter.startDate === null &&
-                  dateFilter.endDate === null
-                ) {
-                  return event;
-                }
-                if (
-                  event.programname
-                    .toLowerCase()
-                    .includes(searchWord.toLowerCase()) ||
-                  event.eventname
-                    .toLowerCase()
-                    .includes(searchWord.toLowerCase())
-                ) {
-                  return event;
-                }
-              })
+          
               .filter((event, index) => {
                 var startDate = new Date(new Date(dateFilter?.startDate).setHours(0))
                 var endDate = new Date(new Date(dateFilter?.endDate).setHours(23))
                 if (startDate !== null && endDate !== null) {
                   let filterPass = true;
-                  const date = new Date(event.eventdate);
+                  const date = new Date(event.supportmeetingdate);
+                  console.log(startDate , date)
                   if (dateFilter.startDate) {
                     filterPass = filterPass && startDate <= date;
                   }
@@ -162,6 +144,8 @@ const SupportGroups = ({ events }) => {
                     filterPass =
                       filterPass && endDate >= date;
                   }
+                  console.log(filterPass)
+
                   return filterPass;
                 }
               })
@@ -190,69 +174,78 @@ const SupportGroups = ({ events }) => {
                         event={event}
                       />
                     </div> */}
-                    <div className="hidden sm:block">
-                      <section
+                    <section className="hidden sm:block">
+                      <div
                         key={index}
-                        className={`grid ${loggedUserRole === "Supervisor" ? "supervisor-existing-events-head-table" : "existing-events-head-table"} px-7 py-7  rounded shadow-md`}
+                        className={`grid ${loggedUserRole === "Supervisor" ? "supervisor-existing-events-head-table" : "existing-events-head-table"} bg-white gap-5 px-7 py-4  rounded-md shadow-md`}
                       >
                         {/* <div className="flex items-center lg:text-xl font-bold ">{event.programname}</div> */}
-                        <div className="flex items-center lg:text-xl font-bold ">
-                          {event.eventname}
-                        </div>
                         <div className="flex items-center lg:text-xl font-bold mr-2">
                           {
-                            event.eventdate &&
-                              new Date(event?.eventdate).toLocaleDateString(
-                                "en-US"
+                            event.supportmeetingdate &&
+                              new Date(event?.supportmeetingdate).toLocaleDateString(
+                                "en-US", {month:"2-digit", day: "2-digit", year: "numeric"}
                               )
                             /* crearFecha2(event) */
                           }
                         </div>
-                        <Link href={`events/${event.id}/nys_cmp/edit`}>
-                          <div className="cursor-pointer flex items-center border-black shadow-md rounded-lg text-center lg:text-xl p-2 font-bold justify-center">
-                            <p className="leading-5">Edit event</p>
-                          </div>
+                        <div className="flex items-center lg:text-xl font-bold truncate">
+                          {event.supportgroupname}
+                        </div>
+                        <div className="flex items-center lg:text-xl font-bold justify-center">
+                          {event.supportgrouptopic}
+                        </div>
+                        <Link href="" >
+                           <a className="flex items-center justify-center">
+                           <img src="/edit-icon.svg" alt="edit event icon" title="Edit event" />
+                           </a>
                         </Link>
                        
-                        {loggedUserRole === "Supervisor" && (
+                        {/* {loggedUserRole === "Supervisor" && ( */}
                           <div className="flex justify-center">
+                            
                             <button
-                              className="bg-black lg:text-lg py-2 px-5 rounded-lg text-white"
-                              onClick={() => handleDeleteEvent(event?.id, event?.eventname)}
+                              className="py-2"
+                              onClick={() => handleDeleteEvent(event?.id, event?.supportgrouptopic)}
+                              title="Delete event"
                             >
-                              Delete event
+                              <img src="/delete-icon.svg"  alt="delete icon" width="25px"/>
                             </button>
                           </div>
-                        )}
-                      </section>
-                    </div>
+                        {/* )} */}
+                      </div>
+                    </section>
                   </>
                 );
               })}
           
         </div>
-        {/* {showDeleteEventModal && (
-          <DeleteEventModal
+         {showDeleteEventModal && (
+          <DeleteSupportGroupEvent
             setShowDeleteEventModal={setShowDeleteEventModal}
             showDeleteEventModal={showDeleteEventModal}
             selectedEventToDelete={selectedEventToDelete}
             id={selectedEventToDelete.id}
           />
-        )} */}
+        )} 
       </div>
+
+      </div>
+      {/*  HEAD TABLE  */}
+      
     </Layout>
   );
 };
 
-export default SupportGroups;
+export default PastEvents;
 
-// export const getServerSideProps = withPageAuthRequired({
-//   async getServerSideProps(ctx) {
-//     const response = await fetch(
-//     //   `${process.env.NEXT_PUBLIC_SERVER_URL}/support_groups`
-//     );
-//     const events = await response.json();
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const response = await fetch(
+       `${process.env.NEXT_PUBLIC_SERVER_URL}/support_groups`
+    );
+    const events = await response.json();
 
-//     return { props: { events } };
-//   },
-// });
+    return { props: { events } };
+  },
+});
