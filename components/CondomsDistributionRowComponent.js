831190ecc8de19c92c02a659196d12755eb1b25b
leoncan122 <leon.can122@gmail.com>
2023-03-15 12:56:38 +0100

    Condoms distribution

--- components/CondomsDistributionRowComponent.js
+++ components/CondomsDistributionRowComponent.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CondomsDistributionRowComponent = ({name,title,setFormData,bg}) => {
   return (
-    <div className={`flex bg-${bg}-blue py-2 rounded-lg my-2 items-center`}>
-      <div className="form-row-item px-5 w-60">
+    <div className={`flex bg-${bg}-pink py-2 rounded-lg my-2 items-center`}>
+      <div className="form-row-item px-5 w-72">
         <p>{title}</p>
       </div>
       <div>
@@ -17,7 +17,7 @@ const CondomsDistributionRowComponent = ({name,title,setFormData,bg}) => {
               [e.target.name]: Number(e.target.value),
             }));
           }}
-          className="p-1 rounded-lg"
+          className="p-2 rounded-lg text-center w-24"
           defaultValue={0}
         />
       </div>
