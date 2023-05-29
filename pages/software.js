11890354ab8c8baa8737d2bbc65e024bb89bbf65
Alexei Garban <garban.valdeon@gmail.com>
2023-05-29 11:23:57 +0200

    new software page design

--- pages/software.js
+++ pages/software.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import BackupDataModal from "../components/BackupDataModal";
+import SubHeader from "../components/SubHeader";
 import backIcon from "../public/BACKicon.svg";
 import Image from "next/image";
 import Layout from "../components/Layout";
@@ -7,6 +8,7 @@ import Link from "next/link";
 import UploadIncidentChecklistModal from "../components/UploadIncidentChecklistModal";
 import BackToDashboardButton from '../components/BackToDashboardButton'
 import BackButton from '../components/BackButton'
+import KeyMetricsSoftware from '../components/KeyMetricsSoftware'
 
 export default function Software() {
   const [dataBackup, setDataBackup] = useState(false);
@@ -23,69 +25,17 @@ These sections are in current development and are on the August - December 2022 
 </p>
 
 </section> */}
-        <section className="bg-light-blue md:h-screen">
-          <div className="container mx-auto pt-10">
-          <div className="flex gap-x-3">
-            <BackButton />
-            <BackToDashboardButton/>
-          </div>
+        
+        <SubHeader pageTitle={"Manage Software"} />
+          <section id="dashboard-client-list" className="">
+            <div className="container mx-auto py-10">
 
-            <div className="grid grid-cols-2 py-5">
-              <h1 className="font-bold px-2">Manage Software</h1>
+            <KeyMetricsSoftware />
 
-              <div className="flex justify-end self-end">
-          </div>
-            </div>
-          </div>
-          <section id="dashboard-client-list">
-            <div className="container mx-auto py-5">
-              <section className=" border-2 border-white shadow mb-10">
-                <div className="supervisor-dashboard-top-container  px-5 py-1 mb-1 bg-white">
-                  <div className="supervisor-dashboard-top  grid md:grid-cols-2  grid-cols-1">
-                    <div>
-                      <h1 className="font-bold my-2">
-                      Key metrics
-                      </h1>
-                    </div>
-                    <div className="flex wrap justify-end items-center">
-                      <div className="px-5 py-1 h-4/6 bg-middle-green w-40 mr-2 flex items-center justify-center">
-                        on-track
-                      </div>
-                      <div className="px-5 py-1 h-4/6 bg-orange w-40 mr-2 flex items-center justify-center">
-                        warning
-                      </div>
-                      <div className="px-5 py-1 h-4/6 bg-light-red w-40 flex items-center justify-center">
-                        alert
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="supervisor-dashboard-top-bottom grid md:grid-cols-4 grid-cols-1 gap-1 bg-white">
-                  <div className="supervisor-dashboard-top-bottom-box bg-white grid items-center">
-                    <div className="px-4 flex py-5">
-                      <img src="./supervisor/software/icon-backup-folder.svg"></img>
-                      <p className="font-semibold text-lg  ml-3">
-                        Days since last backup
-                      </p>
-                    </div>
-                    <h1 className="w-full text-center font-bold py-5 bg-middle-green">
-                      1
-                    </h1>
-                  </div>
-                  <div className="supervisor-dashboard-top-bottom-box bg-white ">
-                    <div className="px-4 flex py-5">
-                      <img src="./supervisor/software/icon-security-incidents-folder.svg"></img>
-                      <p className="font-semibold text-lg  ml-3">
-                        Security incidents or data breaches
-                      </p>
-                    </div>
-                    <h1 className="text-center font-bold py-5 bg-middle-green">
-                      0
-                    </h1>
-                  </div>
-                </div>
-              </section>
-              <h1 className="my-5 container mx-auto text-center md:text-left  lg:pl-0 font-bold">
+
+
+             
+              <h1 className="my-10 container mx-auto text-center md:text-left  lg:pl-0 font-bold">
                 What do you want <span className="bg-yellow px-1"> to do</span>{" "}
                 today?
               </h1>
@@ -155,7 +105,7 @@ These sections are in current development and are on the August - December 2022 
               </div>
             </div>
           </section>
-        </section>
+     
       </Layout>
       {dataBackup && (
         <BackupDataModal
